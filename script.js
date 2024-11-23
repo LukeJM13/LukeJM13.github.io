2a432f83377e5d9d8e6894217fe37ee907eac9c8
Luke Mcfarlane <lmcfarlane97@hotmail.com>
2024-11-23 14:35:36 +0000

    fit page for mobile

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 const copyEmailAddress = (email) => {
-    navigator.clipboard.writeText(email)
+  navigator.clipboard
+    .writeText(email)
     .then(() => {
-        alert('Email copied to clipboard!')
+      alert("Email copied to clipboard!");
     })
     .catch((error) => {
-        console.error('Failed to copy text: ', error);
+      console.error("Failed to copy text: ", error);
     });
 };
 
-window.addEventListener('scroll', () => {
-    const homeBtn = document.getElementById('home-btn');
-    const scrollPosition = window.scrollY;
-  
-    if (scrollPosition > 1000) {
-      homeBtn.classList.add('show');
-    } else {
-      homeBtn.classList.remove('show');
-    }
-  });
+window.addEventListener("scroll", () => {
+  const homeBtn = document.getElementById("home-btn");
+  const work = document.getElementById("work");
+  const projects = document.getElementById("projects");
+  const contact = document.getElementById("contact");
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 300) {
+    work.classList.add("show");
+  } else {
+    work.classList.remove("show");
+  }
+
+  if (scrollPosition > 1000) {
+    homeBtn.classList.add("show");
+  } else {
+    homeBtn.classList.remove("show");
+  }
+
+  if (scrollPosition > 1600) {
+    projects.classList.add("show");
+  } else {
+    projects.classList.remove("show");
+  }
+
+  if (scrollPosition > 1800) {
+    contact.classList.add("show");
+  } else {
+    contact.classList.remove("show");
+  }
+});
